e1ee6d7d21a3dc685d8eeefb1f2c393aab44d848
liudada118 <62827155+liudada118@users.noreply.github.com>
2025-08-05 11:52:15 +0800

    first

---
+++ util/config.js
@@ -0,0 +1,20 @@
+const constantObj = {
+    splitArr: [0xaa, 0x55, 0x03, 0x99],
+    blue : ['robot'] ,
+    order: {
+        1: 'last',
+        2: 'next'
+    },
+    type: {
+        1: 'HL',
+        2: 'HR',
+        3: 'BODY',
+        4: 'ALLBODY',
+        5: 'FL',
+        6: 'FR'
+    },
+    backendAddress: 'http://localhost:3000',
+    blueArr : ['robot']
+}
+
+module.exports = constantObj
